b6021cdc77e0f18fcae61bc985962cec2c3b3a7e
tirthpat1999 <30606850+tirthpat1999@users.noreply.github.com>
2017-11-22 22:57:30 -0500

    Update ECBraking.cpp

--- ECBraking.cpp
+++ ECBraking.cpp
@@ -9,49 +9,46 @@ int main(const int argc, const char* const argv[]){
         if(argc > 4){
             cerr << "Warning: Expecting three command-line arguments; received four; ignoring extraneous arguments." << endl;
         }
-        
-        float v = atof(argv[1]);
-        float d = atof(argv[2]);
-        float maxDecel = abs(atof(argv[3]));
-        float a = abs(pow(v,2)/(2*d));
-        
-        if(d==0 && v!=0){
-            cout << "Crash will occur in 0 seconds at velocity " << abs(v) << "m/s; deploying airbags." << endl;
+        else
+        {
+            collisionCheck(atof(argv[1]), atof(argv[2]), atof(argv[3]));
         }
-        else if((d<0 && v>0) || (d>0 && v<0)){
-            cout << "There is no possibility of collision because the object is always behind the car" << endl;
-        }
-        else if(maxDecel==0){
-            float time = d/v;
-            cout << "Crash will occur in " << time << " seconds at velocity " << v << "m/s; deploying airbags." << endl;
-        }
-        else if(v==0 && d !=0){
-            cout << "There is no possibility of collision because the car is not moving!" << endl;
-        }
-        else{
-            v = abs(v);
-            d = abs(d);
-            
-            float t = v/a;
-
-            if(a > maxDecel){
-                float vf;
-                vf = pow(pow(v,2) - 2*maxDecel*d,0.5);
-                t = (vf - v)/(-1*maxDecel);
-                
-                cout << "Crash will occur in " << t << " seconds at velocity " << vf << " m/s; deploying airbags." << endl;
-            }
-            else{
-                cout << "Initial car velocity: " << v << "m/s" << endl;
-                cout << "Distance to obstacle: " << d << "m" << endl;
-                cout << "Minimum deceleration: " << a << " m/s/s" << endl;
-                cout << "Maximum stopping time: " << t << " s" << endl;
-            }
-        }
-
+       
     }
     else {
         cerr << "Error: insufficient arguments; expecting 3, recieved " << (argc-1) << endl;
     }
     
+}
+void collisionCheck(float velocity, float distance, float maxDeceleration)
+{
+     if ((distance<0 && velocity>0) || (distance>0 && velocity<0))
+        {
+            cout << "There is no possibility of collision because the car is traveling away from the obstacle" << endl;
+        }
+        else if(velocity==0 && distance!=0)
+        {
+            cout << "There is no possibility of collision because the car is not moving" << endl;
+        }
+        else
+        {
+            float minDeceleration= (pow(velocity, 2)/(2*abs(distance)));
+            float stoppingTime= abs(velocity)/abs(minDeceleration);
+            if (abs(minDeceleration) <= abs(maxDeceleration))
+            {
+                cout << "Initial car velocity: " << velocity << "m/s" << endl;  
+                cout << "Distance to obstacle: " << distance << "m/s" << endl;
+                cout << "Minimum deceleration: " << minDeceleration << "m/s/s" << endl;           
+                cout << "Maximum stopping time: " << stoppingTime << "s" << endl;
+            }
+            else
+            {   
+                distance= abs(distance);
+                velocity= abs(velocity);
+
+                float velocityFinal= sqrt((pow(velocity,2)-(2*abs(maxDeceleration)*distance)));
+                stoppingTime= ((velocity-abs(velocityFinal))/abs(maxDeceleration));
+                cout << "Crash will occur in " << stoppingTime << " seconds at velocity " << velocityFinal << "m/s; deploying airbags." << endl;
+            }
+        }    
 }
